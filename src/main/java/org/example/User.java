2a413ac840dc2747b5781e4b24e07f1524d9e5b0
thanujkumarj <thanujkumarj@gmail.com>
2025-01-05 01:12:35 -0500

    Added attributes in user class

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,4 +1,29 @@
 package org.example;
 
 public class User {
+    private final String userId;
+    private String name;
+    private final Account account;
+
+    public User(String userId, String name, Account account){
+        this.name = name;
+        this.userId = userId;
+        this.account = account;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
 }
