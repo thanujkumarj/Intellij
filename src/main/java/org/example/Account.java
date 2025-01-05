29a5a7e9f94bb94577bdff3b395f29e05fb31376
thanujkumarj <thanujkumarj@gmail.com>
2025-01-05 01:25:43 -0500

    Added attributes in Account class

--- src/main/java/org/example/Account.java
+++ src/main/java/org/example/Account.java
@@ -1,4 +1,24 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account {
+    private double balance;
+    private List<String> transactions;
+    public Account(){
+        this.balance = 0.0;
+        this.transactions = new ArrayList<>();
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public List<String> getTransactions() {
+        return transactions;
+    }
+
+
+
 }
